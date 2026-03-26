efae0fe5a1c0f76e91add458d6d2d839165179ef
Max <max781356@gmail.com>
2026-03-27 00:21:55 +0100

    Delete old files (v1.4)

--- src/secfetch/checks/kernel/kernel_version.py
+++
@@ -1,8 +0,0 @@
-import platform
-from secfetch.core.check import security_check
-
-
-# Report running kernel version (informational only)
-@security_check(name="Kernel", category="system", risk="info")
-def check():
-    return {"status": "info", "value": platform.release()}
